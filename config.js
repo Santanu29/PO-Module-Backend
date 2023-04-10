2a62e92f13817bcf42439e5f4b56b6aaf1a89afc
tanmayjain <tanmay.jain@nagarro.com>
2023-04-10 11:27:35 +0530

    Updated 10-04

--- config.js
+++ config.js
@@ -1,23 +1,20 @@
-const AWS = require("aws-sdk");
-var DOC = require("dynamodb-doc");
+const AWS = require('aws-sdk');
+var DOC = require('dynamodb-doc');
 
 const credentials = {
-
-  accessKeyId: "RY4H7AV5",
-
-  secretAccessKey: "D",
-
-  bucketName: "team1backendbucket",
-
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  bucketName: 'team1backendbucket',
 };
+console.log(credentials);
 
 const config = AWS.config.update({
-  region: "ap-south-1",
+  region: 'ap-south-1',
   accessKeyId: credentials.accessKeyId,
   secretAccessKey: credentials.secretAccessKey,
   bucketName: credentials.bucketName,
 });
 
-const podetails = "podetails";
+const podetails = 'podetails';
 var dynamodb = new AWS.DynamoDB();
 module.exports = { config, dynamodb, podetails, credentials };
